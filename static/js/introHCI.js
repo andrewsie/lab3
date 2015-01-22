44cbd79522dbd4024097513a79bbe2207b0941d6
vagrant <vagrant@precise32.(none)>
2015-01-22 22:52:44 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,45 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("I like turtles");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject);
+}
+
+function projectClick(e) {
+    // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the element that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+    console.log("Number of matching items: " + jumbotronHeader.length);
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else if(description.is(':visible')) {
+       description.fadeOut();
+    } else {
+       description.fadeIn();
+    }
+}
+
+function updateProject(e) {
+	var form = $('#projectForm').val();
+
+	$(form).animate({
+		width: $('#width').val()
+	});
+
+	var description = $('#description').val();
+	$(form + '.project-description').text(description);
 }
